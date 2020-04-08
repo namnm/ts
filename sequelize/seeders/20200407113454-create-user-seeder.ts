90a53283d0c477dfa10feb1b902101c09058720a
Hiệp <hoanghiep2402@gmail.com>
2020-04-08 18:35:41 +0700

    Implement migration and seeder for Email (#1)

---
+++ sequelize/seeders/20200407113454-create-user-seeder.ts
@@ -0,0 +1,36 @@
+import { QueryInterface } from 'sequelize'
+
+import { hashPassword } from '../../src-backend/utils/password'
+
+export default {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkInsert(
+      'User',
+      [
+        {
+          primaryEmailId: 1,
+          password: hashPassword('123213'),
+          createdById: 0,
+          createdAt: '2020-04-02 03:06:49',
+        },
+        {
+          primaryEmailId: 2,
+          password: hashPassword('123213'),
+          createdById: 0,
+          createdAt: '2020-04-02 03:06:49',
+        },
+        {
+          primaryEmailId: 3,
+          password: hashPassword('123213'),
+          createdById: 0,
+          createdAt: '2020-04-02 03:06:49',
+        },
+      ],
+      {},
+    )
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkDelete('User', null, {})
+  },
+}
