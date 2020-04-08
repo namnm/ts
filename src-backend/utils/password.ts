194971cd84f75f629801c27e08671300b67808d8
Hiệp <hoanghiep2402@gmail.com>
2020-04-08 16:21:59 +0700

    Implement func hash, compare password (#2)
    * Implement func hash, compare password

---
+++ src-backend/utils/password.ts
@@ -0,0 +1,33 @@
+import { Buffer } from 'buffer'
+import crypto from 'crypto'
+
+const saltLen = 16
+const iterations = 100000
+const keyLen = 64
+const digest = 'sha512'
+
+export const hashPassword = (password: string): string => {
+  const salt = crypto.randomBytes(saltLen)
+  const hash = crypto.pbkdf2Sync(password, salt, iterations, keyLen, digest)
+  const combined = Buffer.alloc(saltLen + keyLen)
+  salt.copy(combined)
+  hash.copy(combined, saltLen)
+  return combined.toString('base64')
+}
+
+export const comparePassword = (
+  password: string,
+  hashedPassword: string,
+): boolean => {
+  const combined = Buffer.from(hashedPassword, 'base64')
+
+  if (combined.length !== saltLen + keyLen) {
+    return false
+  }
+
+  const salt = combined.slice(0, saltLen)
+  const hash = combined.slice(saltLen, combined.length)
+  const hash2 = crypto.pbkdf2Sync(password, salt, iterations, keyLen, digest)
+  const order = Buffer.compare(hash, hash2)
+  return order === 0
+}
