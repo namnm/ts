8668b91a5648af69e1b2d6ef7c72db9ffc767967
Quyen <maihquyen98@gmail.com>
2020-04-24 23:25:07 +0700

    Add model methods

--- src-backend/-/Model.ts
+++ src-backend/-/Model.ts
@@ -1,6 +1,14 @@
-import { Model as Model0, ModelCtor as ModelCtor0 } from 'sequelize'
+import {
+  Model as Model0,
+  ModelCtor as ModelCtor0,
+  WhereOptions,
+} from 'sequelize'
 
 import { ModelAttributes, ToAttributes0, ToModelType } from './types'
+
+type UpdateData<A extends ModelAttributes> = {
+  [k in keyof A]?: ToModelType<A>[k]
+}
 
 export default class Model<A extends ModelAttributes> {
   constructor(private m: ModelCtor0<Model0>) {}
@@ -19,6 +27,48 @@ export default class Model<A extends ModelAttributes> {
       attributes: attrs as string[],
     })
     return (v as unknown) as Pick<ToModelType<A>, K>
+  }
+
+  async findAllWhere<K extends keyof ToModelType<A>>(
+    where?: WhereOptions,
+    attrs?: K[],
+  ) {
+    const v = await this.m.findAll({
+      where,
+      attributes: attrs as string[],
+    })
+    return (v as unknown) as Pick<ToModelType<A>, K>
+  }
+
+  async findOneWhere<K extends keyof ToModelType<A>>(
+    where?: WhereOptions,
+    attrs?: K[],
+  ) {
+    const v = await this.m.findOne({
+      where,
+      attributes: attrs as string[],
+    })
+    return (v as unknown) as Pick<ToModelType<A>, K>
+  }
+
+  async countWhere(where?: WhereOptions) {
+    const v = await this.m.count({ where })
+    return v
+  }
+
+  async existsWhere(where?: WhereOptions) {
+    const v = await this.m.count({ where })
+    return !!v
+  }
+
+  async updateWhere(where: WhereOptions, data: UpdateData<A>) {
+    const [v] = await this.m.update(data, { where })
+    return v
+  }
+
+  async destroyWhere(where: WhereOptions) {
+    const v = await this.m.destroy({ where })
+    return v
   }
 
   // TODO add other model methods here
